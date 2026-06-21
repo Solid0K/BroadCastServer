bad7fec690d56bd1dd62c4932515dd14abf2f9ff
Solid0K <krisu.0396@gmail.com>
2026-06-21 16:48:17 +0530

    Render connection change

--- src/main/java/com/Krishu/Server/BroadCastServer.java
+++ src/main/java/com/Krishu/Server/BroadCastServer.java
@@ -6,7 +6,8 @@ import org.glassfish.tyrus.server.Server;
 
 public class BroadCastServer {
     public static void main(String[] args) throws DeploymentException, InterruptedException {
-        Server server=new Server("localhost",8080,"/",null, BroadCastEndPoint.class);
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT","8080"));
+        Server server = new Server("0.0.0.0", port, "/", null, BroadCastEndPoint.class);
         server.start();
         System.out.println("Server has been started");
         Thread.currentThread().join();
